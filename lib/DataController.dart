c0821800438205a569c06f08eafd256bbbd669d0
Makena Trust <mtrust@crimson.ua.edu>
2022-03-01 18:25:26 -0600

    Fixed search bar. Some bugs.

---
+++ lib/DataController.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class DataController {
+  Future getData(String collection) async {
+    final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    QuerySnapshot snapshot =
+        await firebaseFirestore.collection(collection).get();
+    return snapshot.docs;
+  }
+
+  queryEventData(String queryString) {
+    return FirebaseFirestore.instance
+        .collection('Events')
+        .where('EventName',
+            isEqualTo: queryString.substring(0, 1).toUpperCase())
+        .get();
+  }
+}
